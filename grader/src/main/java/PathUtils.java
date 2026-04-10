6187b23762e983c3cded3811c56495cea7dab1dc
caokhacngoclan <caokhacngoclan@gmail.com>
2026-04-10 21:12:22 +0700

    Revert "Add UI using Swing"
    This reverts commit 113d718ef4f2c29864b7fed8e6c3c3cd5e6d8ef5.

--- grader/src/main/java/PathUtils.java
+++
@@ -1,9 +0,0 @@
-
-
-import java.nio.file.Paths;
-
-public class PathUtils {
-	public static String createFolderPath(Class<?> classPackage, String destination) {
-		return Paths.get(Consts.SRC, Consts.MAIN, Consts.JAVA, classPackage.getPackageName(), Consts.SUBMISSIONS).toString();
-	}
-}
