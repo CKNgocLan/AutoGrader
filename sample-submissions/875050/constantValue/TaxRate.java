3d788f113de193340cef1a7f0b9cec9482951569
caokhacngoclan <caokhacngoclan@gmail.com>
2026-04-11 20:07:44 +0700

    remove unnecessary files

--- sample-submissions/875050/constantValue/TaxRate.java
+++
@@ -1,7 +0,0 @@
-package constantValue;
-
-public class TaxRate {
-    public static final double NORMAL = 0.2;
-    public static final double VIP = 0.1;
-    public static final double STUDENT = 0.0;
-}
